10fb36069ee2b047cb80e28c84f6fe2753791948
Alex Bazhanau <bazhanau@outlook.com>
2022-09-20 14:34:08 +0300

    Connect4.sol draft added

---
+++ contracts/Connect4.sol
@@ -0,0 +1,39 @@
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Connect4 is Ownable {
+    enum GameStatus {
+        Created,
+        InProgress,
+        Complete
+    }
+
+    enum GameResult {
+        CreatorWon,
+        CreatorLost,
+        Draw
+    }
+
+    struct GameRoom {
+        address creator;
+        GameStatus gameStatus;
+        uint256 wagerAmount;
+    }
+
+    mapping(address => uint256) public userPoints;
+
+    GameRoom[] private gameRooms;
+
+    event GameCreated();
+
+    function createGame(uint256 wagerAmount) external {
+        emit GameCreated();
+    }
+
+    function joinGame(uint256 gameId) external {}
+
+    function settleGame(
+        uint256 gameId,
+        GameResult gameResult,
+        uint8[] calldata movesHistory
+    ) external onlyOwner {}
+}
